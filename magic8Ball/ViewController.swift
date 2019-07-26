7c6661a86f7d45936c9e997967408419b472981b
Tadeo Man <imictadeo@gmail.com>
2019-07-25 17:20:52 -0700

    Initial Commit

---
+++ magic8Ball/ViewController.swift
@@ -0,0 +1,37 @@
+//
+//  ViewController.swift
+//  magic8Ball
+//
+//  Created by Michael Tadeo on 7/25/19.
+//  Copyright © 2019 Tadeo Man. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let ballArray = ["ball1", "ball2", "ball3", "ball4", "ball5"]
+    var randomBallNumber = 0
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        newBallImage()
+    }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        newBallImage()
+    }
+    
+    @IBAction func askButtonTapped(_ sender: UIButton) {
+        newBallImage()
+    }
+    
+    func newBallImage() {
+        randomBallNumber = Int(arc4random_uniform(5))
+        imageView.image = UIImage(named: ballArray[randomBallNumber])
+    }
+    
+}
+
